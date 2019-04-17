5012e5d27e78850a32a00d2012db849721507b42
495151742 <495151742@qq.com>
2019-04-17 17:39:45 +0800

    update

--- findMedianSortedArrays.cpp
+++ findMedianSortedArrays.cpp
@@ -4,23 +4,65 @@
 #include <time.h>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <math.h>
 
 using namespace std;
 
 double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
 {
     int i,j = 0;
+    vector<int> q; 
 
-    while(i < nums1.size || j < nums2.size)
+    while(i < nums1.size() || j < nums2.size())
     {
-        /* code */
+        if(i >= nums1.size())
+        {
+            q.push_back(nums2[j]);
+            j++;
+            continue;
+        }
+        
+        if(j >= nums2.size())
+        {
+            q.push_back(nums1[i]);
+            i++;
+            continue;
+        }
+        
+        if(nums1[i] <= nums2[j])
+        {
+            q.push_back(nums1[i]);
+            i++;
+        }
+        else
+        {
+            q.push_back(nums2[j]);
+            j++;
+        }
     }
     
+    int r = q.size()/2;
+
+    if(q.size() % 2 == 0)
+    {
+        return (q[r] + q[r-1])/2.0;
+    }
+    else
+    {
+        return q[r];
+    }   
 }
 
 int main(int argc, char const *argv[])
 {
-    cout << "" << endl;
+    vector<int> a = {2};
+
+    vector<int> b;
+
+    double result = findMedianSortedArrays(a,b);
+
+    cout << result << endl;
 
     return 0;
 }
